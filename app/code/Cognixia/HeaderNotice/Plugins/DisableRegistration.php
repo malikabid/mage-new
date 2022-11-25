444f02dd0c3934a8391256615def154fbd0f2ba0
Abid H. Malik <malikabid2006@gmail.com>
2022-11-25 09:08:09 +0530

    layout updates and registration disable

---
+++ app/code/Cognixia/HeaderNotice/Plugins/DisableRegistration.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ */
+namespace Cognixia\HeaderNotice\Plugins;
+
+use Magento\Customer\Model\Registration;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+/**
+ *
+ */
+class DisableRegistration
+{
+
+    const XML_PATH_DISABLE_CUSTOMER_REGISTRATION = 'customer/create_account/disable_customer_registration';
+
+    protected $scopeConfig;
+
+/**
+ *
+ */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+
+    public function afterIsAllowed(Registration $subject, $result)
+    {
+        if ($this->scopeConfig->isSetFlag(
+            self::XML_PATH_DISABLE_CUSTOMER_REGISTRATION,
+            $scopeType = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+            $scopeCode = null)
+        ){
+            return false;
+        }
+
+        return $result;
+    }
+}
