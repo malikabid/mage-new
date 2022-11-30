29bb036807fcf7344f020f54abeac9a08862708e
Abid H. Malik <malikabid2006@gmail.com>
2022-11-30 17:42:06 +0530

    loaded form data

--- app/code/Cognixia/Blog/Model/Ui/DataProvider/PostDataProvider.php
+++ app/code/Cognixia/Blog/Model/Ui/DataProvider/PostDataProvider.php
@@ -2,15 +2,73 @@
 
 namespace Cognixia\Blog\Model\Ui\DataProvider;
 
+use Cognixia\Blog\Model\ResourceModel\PostFactory;
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Cognixia\Blog\Model\ResourceModel\Post\CollectionFactory;
+use Magento\Framework\App\RequestInterface;
+
 class PostDataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
 {
-    public function addFilter(\Magento\Framework\Api\Filter $filter)
-    {
-        return null;
+    protected $collection;
+    protected $dataPersistor;
+    protected $loadedData;
+    protected $postFactory;
+    protected $request;
+
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        DataPersistorInterface $dataPersistor,
+        PostFactory $postFactory,
+        RequestInterface $request,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->collection = $collectionFactory->create();
+        $this->dataPersistor = $dataPersistor;
+        $this->postFactory = $postFactory;
+        $this->request = $request;
     }
 
     public function getData()
     {
-        return [];
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+
+        $post = $this->getCurrentPost();
+        if (!$post->getData()) {
+            return $post;
+        }
+        $this->loadedData[$post->getId()] = $post->getData();
+
+        return $this->loadedData;
+    }
+
+    private function getCurrentPost()
+    {
+        $postId = $this->request->getParam('id');
+        if ($postId) {
+            try {
+                $post = $this->collection->getFirstItem();
+                $this->loadedData[$post->getId()] = $post->getData();
+            } catch (LocalizedException $exception) {
+                $post = $this->postFactory->create();
+            }
+
+            return $post;
+        }
+
+        $data = $this->dataPersistor->get('blog_post');
+        if (empty($data)) {
+            return $this->postFactory->create();
+        }
+        $this->dataPersistor->clear('blog_post');
+
+        return $this->postFactory->create()
+            ->setData($data);
     }
 }
