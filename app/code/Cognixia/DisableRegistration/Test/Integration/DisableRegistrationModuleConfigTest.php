20d5c94dd50c0faaaf01ed3f3679420b479e6e26
Abid H. Malik <malikabid2006@gmail.com>
2022-12-03 13:44:20 +0530

    module registered test

---
+++ app/code/Cognixia/DisableRegistration/Test/Integration/DisableRegistrationModuleConfigTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cognixia\DisableRegistration\Test\Integration;
+
+use Magento\Framework\Component\ComponentRegistrar;
+use Magento\Framework\Module\ModuleList;
+use Magento\TestFramework\ObjectManager;
+use PHPUnit\Framework\TestCase;
+
+class DisableRegistrationModuleConfigTest extends TestCase
+{
+    private $modulename = "Cognixia_DisableRegistration";
+
+    public function testTheModuleIsRegistered()
+    {
+        $registrar = new ComponentRegistrar();
+        $this->assertArrayHasKey($this->modulename, $registrar->getPaths(ComponentRegistrar::MODULE));
+    }
+
+    public function testTheModuleIsConfiguredAndEnabled()
+    {
+        $objectManager = ObjectManager::getInstance();
+
+        $moduleList = $objectManager->create(ModuleList::class);
+
+        $isModuleListed = $moduleList->has($this->modulename);
+        $this->assertTrue($isModuleListed);
+    }
+}
